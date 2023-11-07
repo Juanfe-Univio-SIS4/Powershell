1238297040e42f5ac4c2513c7fc1894355fe13ee
Juanfe-Univio-SIS4 <junivioacero.cf@iesesteveterradas.cat>
2023-11-07 15:44:24 +0100

    Afegim les activitats a github. Pugem Activitat 6

---
+++ Actividad_6/Actividad 6 - Ej1.ps1
@@ -0,0 +1,21 @@
+﻿$RutaBusqueda = Read-Host "Introduce la ruta de busqueda (ex: C:\Ruta\Raiz):"
+
+if (Test-Path -Path $RutaBusqueda) {
+
+    $Tipo = Read-Host "Vols buscar ficheros o directorios? (Documento*/Carpeta de archivos):"
+
+
+    $Resultados = Get-ChildItem -Path $RutaBusqueda -Recurse -File | Where-Object { $_.PSIsContainer -eq ($Tipo -eq 'directorios') }
+
+    if ($Resultados.Count -gt 0) {
+ 
+        Write-Host "Los siguientes $Tipo se han encontrado en la ruta $RutaBusqueda :"
+        $Resultados | ForEach-Object {
+            Write-Host $_.FullName
+        }
+    } else {
+        Write-Host "No se han encontrado $Tipo en la ruta $RutaBusqueda."
+    }
+} else {
+    Write-Host "La ruta especificada no existe."
+}
---
+++ Actividad_6/Actividad 6 - Ej2.ps1
@@ -0,0 +1,13 @@
+﻿$Ruta = Read-Host "Introduce la ruta absoluta del directorio que deseas copiar"
+
+if (Test-Path -Path $Ruta){
+    $Destino = Read-Host "Introduce la ruta absoluta del directorio donde se guardara la copia de seguridad"
+    if (Test-Path -Path $Destino){
+        Copy-Item -Path $Ruta -Destination $Destino -Recurse
+        Write-Host "Copia completada"
+    }else{
+        Write-Host "El directorio de destino no se encuentra"
+    }
+}else{
+    Write-Host "El directorio a copiar no existe"
+}
